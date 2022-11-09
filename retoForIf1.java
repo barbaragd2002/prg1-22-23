009ce80105fefa47d8262509765dcd77e97d5a64
barbaragd2002 <74409609+barbaragd2002@users.noreply.github.com>
2022-11-09 19:05:35 +0100

    RetoForIf

---
+++ retoForIf1.java
@@ -0,0 +1,22 @@
+public class retoForIf1 {
+    public static void main(String[] args) throws Exception {
+        int dimension;
+        dimension = 21;
+
+        for (int j = 1; j <= dimension; j = j + 1 ) {
+            for(int i = 1; i <= dimension; i = i + 1){
+                if(
+                    //El codigo desde aqui
+                    i == 2 && j == 2
+                    //El codigo hasta aquí
+                ){
+                    System.out.print("(*)");
+                }else{
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+            
+        }
+    }
+} 
